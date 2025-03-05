80cc30aba7ee48de21a9bfa562b9b655d7cb4337
Fadhil Hussain <ibnhussainkv@gmail.com>
2025-03-05 10:51:51 +0530

    web application

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,11 @@
+import sys
+from src.logger import logging
+from src.exception import CustomException
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+class NewData:
+    def __init__(self):
+        pass
