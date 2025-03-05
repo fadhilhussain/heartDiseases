61e762ee0a792cb1c0b1745b8af0fa4ef0bb24b2
Fadhil Hussain <ibnhussainkv@gmail.com>
2025-03-05 14:17:36 +0530

    CHD

--- app.py
+++ app.py
@@ -1,60 +1,59 @@
 from flask import Flask, render_template,request,url_for,send_file
 from fpdf import FPDF
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import PredictPipeline,NewData
 
 
 application = Flask(__name__)
 app = application
 
-grid_model = pickle.load(open('models/grid1.pkl','rb'))
-scaler = pickle.load(open('models/scaler1.pkl','rb'))
-
 
 @app.route('/',methods = ['GET','POST'])
 def index():
     if request.method == 'POST':
-        gender = int(request.form.get('gender'))
-        age = int(request.form.get('age'))
-        education = float(request.form.get('education'))
-        currentSmoker = int(request.form.get('currentSmoker'))
-        cigsPerDay = int(request.form.get('cigsPerDay'))
-        prevalentHyp = int(request.form.get('prevalentHyp'))
-        totChol = int(request.form.get('totChol'))
-        sysBP = float(request.form.get('sysBP'))
-        BMI = float(request.form.get('BMI'))
-        heartRate = float(request.form.get('heartRate'))
-        glucose = float(request.form.get('glucose'))
+        data = NewData(
+            gender = int(request.form.get('gender')),
+            age = int(request.form.get('age')),
+            education = float(request.form.get('education')),
+            currentSmoker = int(request.form.get('currentSmoker')),
+            cigsPerDay = int(request.form.get('cigsPerDay')),
+            prevalentHyp = int(request.form.get('prevalentHyp')),
+            totChol = int(request.form.get('totChol')),
+            sysBP = float(request.form.get('sysBP')),
+            BMI = float(request.form.get('BMI')),
+            heartRate = float(request.form.get('heartRate')),
+            glucose = float(request.form.get('glucose'))
+        )
 
-        new_scaled = scaler.transform([[gender,age,currentSmoker,cigsPerDay,BPmeds,prevalentHyp,sysBP,diaBP,heartRate,glucose]])     
-        # probs = grid_model.predict_proba(new_scaled)
-        result = grid_model.predict(new_scaled)
+        prediction_data = data.get_dataFrame()
+        print(prediction_data)
+
+        #iniilizing prediction pipeline 
+        predict_pipline = PredictPipeline()
+        result = predict_pipline.predict(prediction_data)
 
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font('Arial',size=12)
         pdf.cell(200, 10, txt="CHD Prediction Report", ln=True, align='C')
         pdf.ln(10)
-        pdf.cell(200, 10, txt=f"Gender: {gender}", ln=True)
-        pdf.cell(200, 10, txt=f"Age: {age}", ln=True)
-        pdf.cell(200, 10, txt=f"Current Smoker: {currentSmoker}", ln=True)
-        pdf.cell(200, 10, txt=f"Cigarettes Per Day: {cigsPerDay}", ln=True)
-        pdf.cell(200, 10, txt=f"BP Medications: {BPmeds}", ln=True)
-        pdf.cell(200, 10, txt=f"Prevalent Hypertension: {prevalentHyp}", ln=True)
-        pdf.cell(200, 10, txt=f"Systolic BP: {sysBP}", ln=True)
-        pdf.cell(200, 10, txt=f"Diastolic BP: {diaBP}", ln=True)
-        pdf.cell(200, 10, txt=f"Heart Rate: {heartRate}", ln=True)
-        pdf.cell(200, 10, txt=f"Glucose: {glucose}", ln=True)
+        pdf.cell(200, 10, txt=f"Gender: {data.gender}", ln=True)
+        pdf.cell(200, 10, txt=f"Age: {data.age}", ln=True)
+        pdf.cell(200, 10, txt=f"Education: {data.education}", ln=True)
+        pdf.cell(200, 10, txt=f"Current Smoker: {data.currentSmoker}", ln=True)
+        pdf.cell(200, 10, txt=f"Cigarettes Per Day: {data.cigsPerDay}", ln=True)
+        pdf.cell(200, 10, txt=f"Prevalent Hypertension: {data.prevalentHyp}", ln=True)
+        pdf.cell(200, 10, txt=f"Total Cholostrol: {data.totChol}", ln=True)
+        pdf.cell(200, 10, txt=f"Systolic BP: {data.sysBP}", ln=True)
+        pdf.cell(200, 10, txt=f"BMI: {data.BMI}", ln=True)
+        pdf.cell(200, 10, txt=f"Heart Rate: {data.heartRate}", ln=True)
+        pdf.cell(200, 10, txt=f"Glucose: {data.glucose}", ln=True)
         pdf.ln(10)
         if result[0] == 0:
             res = ' NO CHD !'
         elif result[0] == 1:
             res = ' YES CHD '
         pdf.set_text_color(255, 0, 0)
-        pdf.cell(200, 10, txt=f"Prediction: {res}", ln=True)
-        # pdf.cell(200, 10, txt=f"Probabilites towords to happen CHD: {probs}", ln=True)
+        pdf.cell(200, 10, txt=f"{res}", ln=True)
         pdf_path = "CHD_Prediction_Report.pdf"
         pdf.output(pdf_path)
         return send_file(pdf_path, as_attachment=True)
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,11 +1,71 @@
 import sys
+import pandas as pd 
 from src.logger import logging
 from src.exception import CustomException
+from src.utils import load_object
 
 class PredictPipeline:
     def __init__(self):
         pass
 
+    def predict(self,features):
+        try:
+            preprocessing_path = 'artifacts/preprocessor.pkl'
+            model_path = 'artifacts/model.pkl'
+
+            #file initilizing 
+            preprocessor = load_object(preprocessing_path)
+            model = load_object(model_path)
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
+
 class NewData:
-    def __init__(self):
-        pass
+    def __init__(self,
+            gender,
+            age,
+            education,
+            currentSmoker,
+            cigsPerDay,
+            prevalentHyp,
+            totChol,
+            sysBP,
+            BMI,
+            heartRate,
+            glucose):
+        
+        self.gender = gender
+        self.age = age
+        self.education = education
+        self.currentSmoker = currentSmoker
+        self.cigsPerDay = cigsPerDay
+        self.prevalentHyp = prevalentHyp
+        self.totChol = totChol
+        self.sysBP = sysBP
+        self.BMI = BMI
+        self.heartRate = heartRate
+        self.glucose = glucose
+
+    def get_dataFrame(self):
+        try:
+            dataframe_dict = {
+                'gender' : [self.gender],
+                'age' : [self.age],
+                'education' : [self.education],
+                'currentSmoker' : [self.currentSmoker],
+                'cigsPerDay' : [self.cigsPerDay],
+                'prevalentHyp' : [self.prevalentHyp],
+                'totChol' : [self.totChol],
+                'sysBP' : [self.sysBP],
+                'BMI' : [self.BMI],
+                'heartRate' : [self.heartRate],
+                'glucose' : [self.glucose]
+            }
+            return pd.DataFrame(dataframe_dict)
+        
+        except Exception as e:
+            raise CustomException(e,sys)
